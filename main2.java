f3c837b466b9ecd036ef55e5084eb1baea50b491
Lucas Gabriel <106352894+LuckeG@users.noreply.github.com>
2024-11-28 21:14:18 -0300

    main2.java

---
+++ main2.java
@@ -0,0 +1,17 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws ParseException {
+        Scanner leitor = new Scanner(System.in);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+        Date checkin = sdf.parse("11-11-2024");
+        Date checkout = sdf.parse("12-11-2024");
+        Reserva r1 = new Reserva(4, checkin, checkout);
+
+        r1.exibirReserva();
+    }
+}
