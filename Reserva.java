d42c37c35fcf4b467d81fbc41aa08643c0ba6364
Lucas Gabriel <106352894+LuckeG@users.noreply.github.com>
2024-11-28 21:17:55 -0300

    Reserva.java

---
+++ Reserva.java
@@ -0,0 +1,52 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class Reserva {
+    private int numeroQuarto;
+    private Date checkIn;
+    private Date checkOut;
+    private SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+
+    public Reserva(int numeroQuarto, Date checkIn, Date checkOut) {
+        this.numeroQuarto = numeroQuarto;
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+    }
+
+    public Date getCheckIn() {
+        return checkIn;
+    }
+
+    public int getNumeroQuarto() {
+        return numeroQuarto;
+    }
+
+    public Date getCheckOut() {
+        return checkOut;
+    }
+
+    public void setNumeroQuarto(int numeroQuarto) {
+        this.numeroQuarto = numeroQuarto;
+    }
+
+    public void setCheckIn(Date checkIn) {
+        this.checkIn = checkIn;
+    }
+
+    public void setCheckOut(Date checkOut) {
+        this.checkOut = checkOut;
+    }
+
+    public long duracao(){
+        long dia = checkOut.getTime()-checkIn.getTime();
+        return TimeUnit.DAYS.convert(dia,TimeUnit.MILLISECONDS);
+    }
+
+    public void exibirReserva(){
+        System.out.println("Quarto: "+numeroQuarto);
+        System.out.println("CheckIn: "+checkIn);
+        System.out.println("CheckOut: "+checkOut);
+        System.out.println("Duração: "+duracao());
+    }
+}
